e90111c7e3b514e7d72508dc05147c2a7c641470
kitchenhoods <147158984+kitchenhoods@users.noreply.github.com>
2023-10-06 14:22:53 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+// config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDSmCV-iouxRtpY7DWMxCLkHachjS-doRs",
+
+  authDomain: "fortressdesignwebsite4builder.firebaseapp.com",
+
+  databaseURL:
+    "https://fortressdesignwebsite4builder-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "fortressdesignwebsite4builder",
+
+  storageBucket: "fortressdesignwebsite4builder.appspot.com",
+
+  messagingSenderId: "551735404686",
+
+  appId: "1:551735404686:web:e370ef53425173936e41df",
+
+  measurementId: "G-3GCLZE78BJ",
+};
